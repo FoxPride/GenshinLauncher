8610cf8ce571c3202f9c67eebea0066b34c8577f
FoxPride <c1ee3517@opayq.com>
2022-05-21 03:19:31 +0300

    Versions update
    - Update framework version
    - Update libraries
    - Code cleanup

--- GenshinLauncher/App.xaml.cs
+++ GenshinLauncher/App.xaml.cs
@@ -1,11 +1,9 @@
-﻿using System.Windows;
-
-namespace GenshinLauncher
+﻿namespace GenshinLauncher
 {
     /// <summary>
     ///     Interaction logic for App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App
     {
     }
 }
